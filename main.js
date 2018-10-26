d268a44cccf2b6b748221899f406986880e2f682
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-10-26 23:46:07 +0100

    created the key generation file

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ let nickCoin = new Blockchain();
 nickCoin.createTransaction(new Transaction("address1", "address2", 10));
 nickCoin.createTransaction(new Transaction("address1", "address2", 5));
 
+nickCoin.minePendingTransactions("Nick-address");
+nickCoin.minePendingTransactions("Nick-address");
+nickCoin.minePendingTransactions("Nick-address");
+console.log(nickCoin.getBalanceOfAddress("Nick-address"));
+
 
 
 
