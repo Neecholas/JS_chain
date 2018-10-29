77d8c14261f8204ae22847d5e22b1c44965e400c
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-10-29 13:20:56 +0000

    Created a signature checking functiono

--- block.js
+++ block.js
@@ -41,10 +41,24 @@ class Transaction {
   }
 
   signTransaction(keySign){
+    if(signingKey.getPublic('hex') !== this.fromAddress){
+      throw new Error('You cannot sign transactions for orther wallets!');
+    }
 
     const hashTran = this.calculateHash();
     const sig = keySign.sign(hashTran, 'base64');
     this.signature = sig.toDER('hex');
+  }
+
+  isReal(){
+    if(this.fromAddress === null) return true;
+
+    if(!this.signature || this.signature.length === 0){
+      throw new Error('No signature in this transaction');
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
