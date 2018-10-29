ea87d6c6098b8affb618c81b9815a55404430123
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-10-29 10:42:59 +0000

    annotated keyGen.js

--- keyGen.js
+++ keyGen.js
@@ -1,12 +1,15 @@
 var EC = require('elliptic').ec;
-// const ec = new EC('secp256k1');
+// creates a new elliptic curve using the secp256k1 algorithm
+const ec = new EC('secp256k1');
 
-// const key = ec.genKeyPair();
-// const publicKey = key.getPublic('hex');
-// const privateKey = key.getPrivate('hex');
+// generate a keypair
+const key = ec.genKeyPair();
+// create public and private keys in hexadecimal format
+const publicKey = key.getPublic('hex');
+const privateKey = key.getPrivate('hex');
 
-// console.log();
-// console.log('Private key:', privateKey);
+console.log();
+console.log('Private key:', privateKey);
 
-// console.log();
-// console.log('Public key:', publicKey);
+console.log();
+console.log('Public key:', publicKey);
